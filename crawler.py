29c284e899df7f3badaced383fda3ac765150f14
Murali S <muralisc@gmail.com>
2019-08-04 19:57:03 +0530

    Refactor server

--- crawler.py
+++ crawler.py
@@ -56,38 +56,42 @@ class Crawler:
         self.depth = depth
         self.parsed_root_url = Url(self.root_url)
 
+    def process_link(self, cur_url, cur_depth, link_url):
+        try:
+            parsed_link = Url(link_url, self.root_url)
+        except:
+            logger.error("Error while creating Url: "+ str(link))
+        if self.parsed_root_url.isSameDomain(parsed_link):
+            logger.debug("Adding to map {self.root_url} {parsed_link.url}")
+            if parsed_link.parsed_url.path not in self.site_map[cur_url]:
+                self.site_map[cur_url].append(parsed_link.parsed_url.path)
+            if cur_depth > 1:
+                self.queue.append( ( parsed_link.get_full_url() , cur_depth-1) )
+
+    def visit_url(self, cur_url, cur_depth):
+        if self.visited[cur_url] == False:
+            logger.info("Visiting: {} at depth {}".format(cur_url, cur_depth))
+            fp = request.urlopen(cur_url)
+            self.visited[cur_url] = True
+            mybytes = fp.read()
+            mystr = str(mybytes)
+            fp.close()
+            soup = BeautifulSoup( mystr, "html.parser")
+            links = soup.find_all("a")
+            for link in links:
+                link_url = link.get('href')
+                self.process_link(cur_url, cur_depth, link_url)
+        else:
+            logger.info("Skipping: {} ".format(cur_url))
+
     def crawl(self):
-        visited = defaultdict(lambda: False)
-        queue = []
+        self.visited = defaultdict(lambda: False)
+        self.queue = []
         logger.debug("Crawling " + self.root_url)
-        queue.append( (self.root_url, self.depth) )
-        while len(queue) > 0:
-            root_tuple = queue.pop(0)
+        self.queue.append( (self.root_url, self.depth) )
+        while len(self.queue) > 0:
+            root_tuple = self.queue.pop(0)
             cur_url = root_tuple[0]
             cur_depth = root_tuple[1]
-            if visited[cur_url] == False:
-                logger.info("Visiting: {} at depth {}".format(cur_url, cur_depth))
-                fp = request.urlopen(cur_url)
-                visited[cur_url] = True
-                mybytes = fp.read()
-                try:
-                    mystr = mybytes.decode("utf8")
-                except UnicodeDecodeError as e:
-                    logger.error("Unicode decode error occured for url: " + cur_url)
-                fp.close()
-                soup = BeautifulSoup( mystr, "html.parser")
-                links = soup.find_all("a")
-                for link in links:
-                    link_url = link.get('href')
-                    try:
-                        parsed_link = Url(link_url, self.root_url)
-                    except:
-                        logger.error("Error while creating Url: "+ str(link))
-                    if self.parsed_root_url.isSameDomain(parsed_link):
-                        logger.debug("Adding to map {self.root_url} {parsed_link.url}")
-                        self.site_map[cur_url].append(parsed_link.parsed_url.path)
-                        if cur_depth > 1:
-                            queue.append( ( parsed_link.get_full_url() , cur_depth-1) )
-            else:
-                logger.info("Skipping: {} ".format(cur_url))
+            self.visit_url(cur_url, cur_depth)
         return self.site_map
--- server.py
+++ server.py
@@ -3,6 +3,7 @@
 import crawler
 from flask import Flask, jsonify, request
 import logging
+import json
 
 log_level = logging.DEBUG
 logging.basicConfig(level=log_level)
@@ -16,7 +17,11 @@ def hello_world():
     depth =  request.form['depth']
     logger.info("Recieved request for url: "+url+ " depth: " + depth )
     c = crawler.Crawler(url, int(depth))
-    site_map = c.crawl()
+    try:
+        site_map = c.crawl()
+    except:
+        logger.error("Error while crawling", exc_info=True)
+        return json.dumps('{ "message" : "Server error"}')
     return jsonify( dict(site_map) )
 
 if __name__ == '__main__':
